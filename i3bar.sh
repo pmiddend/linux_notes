1bc04bfc8d07b6ce7c784829527765edd72e5207
Philipp Middendorf <pmidden@secure.mailbox.org>
2016-03-16 22:09:12 +0100

    mail fix

--- i3bar.sh
+++ i3bar.sh
@@ -7,6 +7,6 @@ do
     read line
     memory_tick=$(free | grep Mem | awk '{print int($3/$2 * 8)}')
     memory_percent=$(free | grep Mem | awk '{print int($3/$2 * 100)}')
-    unread_mail_count=$(mu find 'flag:unread AND NOT flag:trashed' 2>/dev/null | wc -l)
+    unread_mail_count=$(mu find flag:unread AND NOT flag:trashed 2>/dev/null | wc -l)
     echo "✉ ${unread_mail_count} | 🖥  ${ticks[$memory_tick]} ${memory_percent}% | $line" || exit 1
 done
