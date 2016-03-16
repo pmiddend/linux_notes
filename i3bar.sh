2cd55d79d5325dfab96b4743ed9981426dddbc3e
Philipp Middendorf <pmidden@secure.mailbox.org>
2016-03-16 22:00:39 +0100

    move time, add mail

--- i3bar.sh
+++ i3bar.sh
@@ -6,6 +6,7 @@ i3status --config ~/.i3status.conf | while :
 do
     read line
     memory_tick=$(free | grep Mem | awk '{print int($3/$2 * 8)}')
-    memory_percent=$(free | grep Mem | awk '{print int($3/$2 * 100)}')    
-    echo "🖥  ${ticks[$memory_tick]} ${memory_percent}% | $line" || exit 1
+    memory_percent=$(free | grep Mem | awk '{print int($3/$2 * 100)}')
+    unread_mail_count=$(mu find 'flag:unread AND NOT flag:trashed' 2>/dev/null | wc -l)
+    echo "✉ ${unread_mail_count} | 🖥  ${ticks[$memory_tick]} ${memory_percent}% | $line" || exit 1
 done
